55c0d46b4d8a2a2c61869cf411035f68b1db1c0a
Emily Tomas Vialogo <emily.vialogo@akross.com.br>
2023-11-24 00:25:48 -0300

    AUTH - Login e Logout utilizando Firebase

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
-document.getElementById("loginButton").addEventListener("click", function() {
-    var username = document.getElementById("Nome").value;
-    var password = document.getElementById("Senha").value;
-    if (username === "seu_usuario" && password === "sua_senha") {
-        window.location.href = "jogo.html";
-    } else {
-        alert("Credenciais inválidas. Tente novamente.");
-    }
-});
-function redirecionarParaJogo() {
-    // Redireciona para a página "jogo.html"
-    window.location.href = "jogo.html";
-}
+// document.getElementById("loginButton").addEventListener("click", function() {
+//     var username = document.getElementById("Nome").value;
+//     var password = document.getElementById("Senha").value;
+//     if (username === "seu_usuario" && password === "sua_senha") {
+//         window.location.href = "jogo.html";
+//     } else {
+//         alert("Credenciais inválidas. Tente novamente.");
+//     }
+// });
+// function redirecionarParaJogo() {
+//     // Redireciona para a página "jogo.html"
+//     window.location.href = "jogo.html";
+// }
